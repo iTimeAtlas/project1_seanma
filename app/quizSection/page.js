100c207b55fd58e94f55ff22b6a1ddab47ca07af
iTimeAtlas <sean.cuong.ma@gmail.com>
2024-12-03 19:35:05 -0700

    First Upload

---
+++ app/quizSection/page.js
@@ -0,0 +1,26 @@
+
+export default function QuizPage() {
+
+
+    return(
+        <div>
+            <header>
+                <h1>
+                    QUIZ TIME!
+                </h1>
+            </header>
+            <form>
+                <div>
+                    <label>Your name: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>2 + 2: </label>
+                    <input type="number"/>
+                </div>
+                
+
+            </form>
+        </div>
+    );
+}
